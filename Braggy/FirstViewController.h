d0bf0bd26bae05305604559a0a0eb4e5a162b9c7
Tongbram <rtongbram@paypal.com>
2014-07-24 12:31:36 -0700

    Initial Commit

---
+++ Braggy/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Braggy
+//
+//  Created by Tongbram, Robertis on 7/24/14.
+//  Copyright (c) 2014 Tongbram, Robertis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Braggy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Braggy
+//
+//  Created by Tongbram, Robertis on 7/24/14.
+//  Copyright (c) 2014 Tongbram, Robertis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
